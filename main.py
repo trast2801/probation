151e2a87ced5fffaf8f1cfc43b9d33af0b1b55a6
@trast2801 <trast2801@yandex.ru>
2024-10-16 17:22:24 +0300

    Добавлена сообщение об изменении цены выше норматива

--- main.py
+++ main.py
@@ -1,27 +1,59 @@
+from time import sleep
+
 import data_download as dd
 import data_plotting as dplt
-import solution as cd
+import solution as sl
+import freestyle as fr
 
 
-
-def main():
+def first():
     print("Добро пожаловать в инструмент получения и построения графиков биржевых данных.")
-    print("Вот несколько примеров биржевых тикеров, которые вы можете рассмотреть: AAPL (Apple Inc), GOOGL (Alphabet Inc), MSFT (Microsoft Corporation), AMZN (Amazon.com Inc), TSLA (Tesla Inc).")
-    print("Общие периоды времени для данных о запасах включают: 1д, 5д, 1мес, 3мес, 6мес, 1г, 2г, 5г, 10л, с начала года, макс.")
+    print(
+        "Вот несколько примеров биржевых тикеров, которые вы можете рассмотреть: AAPL (Apple Inc), GOOGL (Alphabet Inc), MSFT (Microsoft Corporation), AMZN (Amazon.com Inc), TSLA (Tesla Inc).")
+    print(
+        "Общие периоды времени для данных о запасах включают: 1д, 5д, 1мес, 3мес, 6мес, 1г, 2г, 5г, 10л, с начала года, макс.")
 
     ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc):»")
     period = input("Введите период для данных (например, '1mo' для одного месяца): ")
 
     # Fetch stock data
     stock_data = dd.fetch_stock_data(ticker, period)
-    middle = cd.calculate_and_display_average_price(stock_data)
+
+    middle = sl.calculate_and_display_average_price(stock_data)
+    otklonenie = sl.notify_if_strong_fluctuations(stock_data, 5)
     # Add moving average to the data
     stock_data = dd.add_moving_average(stock_data)
 
     # Plot the data
     dplt.create_and_save_plot(stock_data, ticker, period)
 
-    print(f'Среднее за период {middle}')
+
+
+    print(f'Среднее за период: {middle}\n'
+          f'Отклонение выше норматива: {otklonenie}')
+
+def second():
+    ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc):»")
+    ticker="AAPL"
+    print("Переходим в режим ежеминутной прорисовки графика, для выхода нажмите (CTRL+C)")
+    while True:
+        stock_data = fr.load_free_data(ticker)
+        stock_data = dd.add_moving_average(stock_data)
+        fr.create_and_save_plot_fr(stock_data, ticker, period='1mo')
+
+        print("ddd")
+
+def main():
+    while True:
+        key = input("Выберите режим (1 - по ТЗ, 2 - Вольное творчество, 3 - выход) : ")
+        #key="2"
+        if key == "1":
+            first()
+        if key == "2":
+            second()
+        if key == "3":
+            break
+
 
 if __name__ == "__main__":
     main()
--- solution.py
+++ solution.py
@@ -1,4 +1,18 @@
 import pandas as pd
+from datetime import date
+import yfinance as yf
 
 def calculate_and_display_average_price(data):
+    '''Функция выводит среднюю цену закрытия акций за заданный период'''
     return data['Close'].mean()
+
+
+def notify_if_strong_fluctuations(data, threshold):
+    '''Функция анализирует данные и уведомляет пользователя,
+    если цена акций изменялась более чем на заданный процент за период'''
+
+    percent = (data['Close'].max() - data['Close'].min()) / data['Close'].mean() * 100
+    if percent >= threshold:
+        #print(f'цена акций колебалась на {percent}%')
+        return percent
+    return None
